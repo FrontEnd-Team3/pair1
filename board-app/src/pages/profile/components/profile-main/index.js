8b973c932f11dcc4b51ae0776ae38f6b4baa4747
ohwphil <ohwphil@gmail.com>
2023-05-28 01:19:16 +0900

    feat: 회원정보 수정 로직 추가

--- board-app/src/pages/profile/components/profile-main/index.js
+++ board-app/src/pages/profile/components/profile-main/index.js
@@ -1,9 +1,23 @@
+import styled from "styled-components";
+import { useUserInfo } from "../../../../context/user-info";
+
 const ProfileMain = () => {
+  const [userState, setUserState] = useUserInfo();
   return (
-    <div>
-      <h1>이름: {"User1"}</h1>
-    </div>
+    <S.ProfileContainer>
+      <h1>프로필 정보</h1>
+      <p>닉네임: {userState.username}</p>
+      <p>이메일: {userState.email}</p>
+      <p>게시글 수: 12</p>
+      <p>추천 수: 123</p>
+    </S.ProfileContainer>
   );
 };
-
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 300px);
+  align-items: center;
+`;
 export default ProfileMain;
+const S = { ProfileContainer };
